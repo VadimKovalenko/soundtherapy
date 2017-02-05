254a9d94cab8f6ba59047caf5ad24be7857232c2
Vadim Kovalenko <vadblaxmith@gmail.com>
2017-02-05 17:45:15 +0200

    second commit

---
+++ wp-content/themes/sound_t/template-parts/content-home.php
@@ -0,0 +1,11 @@
+﻿<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package sound_t
+ */
+
+?>
+<?php the_content(); ?>
