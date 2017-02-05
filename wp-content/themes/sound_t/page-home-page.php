b7091afb27b5fe475d1dd9a2a63b5c68198f9288
Vadim Kovalenko <vadblaxmith@gmail.com>
2017-02-05 18:03:26 +0200

    third commit

--- wp-content/themes/sound_t/page-home-page.php
+++ wp-content/themes/sound_t/page-home-page.php
@@ -13,15 +13,13 @@
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
-	<h2>главный лендинг</h2>
 		<main id="main" class="site-main" role="main">
-
 			<?php
+			while ( have_posts() ) : the_post();
 				get_template_part( 'template-parts/content', 'home' );
+			endwhile; // End of the loop.
 			?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
